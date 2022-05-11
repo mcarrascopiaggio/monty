2813ece4999a1f5419736fba6b7a9cd3552af1e6
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-11 11:12:22 -0700

    include aschi 127

--- 3-opcode.c
+++ 3-opcode.c
@@ -69,7 +69,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+	if ((*stack)->n < 0 || (*stack)->n >= 128)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 	}
