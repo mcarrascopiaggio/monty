a820925b6f9a6184a5b3906494c8cd3a74843b48
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-11 11:16:33 -0700

    change message error one character

--- 3-opcode.c
+++ 3-opcode.c
@@ -64,14 +64,15 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n < 0 || (*stack)->n >= 128)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
 }
